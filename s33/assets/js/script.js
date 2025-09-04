bd819a05ddbf9500e3fe961c05e0279a83c71410
valee_pky <valvillarrealguardado@crackthecode.la>
2025-09-04 09:58:29 -0500

    s33

---
+++ s33/assets/js/script.js
@@ -0,0 +1,22 @@
+let mainTitle = document.getElementById("main-title");
+console.log(mainTitle);
+
+console.log(mainTitle.innerText);
+
+mainTitle.innerText = "TITULO CAMBIADO";
+console.log(mainTitle.innerText);
+
+console.log("SELECCIONAR POR CLASE");
+
+let mainText = document.getElementsByClassName("main-text");
+console.log(mainText);
+
+console.log(mainText[0].innerText);
+console.log(mainText[2].innerText);
+
+mainText[1].innerText = "lorem";
+
+console.log("SELECCIONAR POR ETIQUETA");
+let title = document.getElementsByTagName("h3");
+console.log(title);
+console.log(title[2].innerText);
