d98b0e2e869313c825d39c9a28d01fc55acf6706
valee_pky <valvillarrealguardado@crackthecode.la>
2025-09-05 09:09:37 -0500

    s34

---
+++ s34/assets/js/script.js
@@ -0,0 +1,14 @@
+//METODOS DE ARRAYS
+
+let fruits = ["manzana", "naranja", "pera", "mango"];
+console.log(fruits);
+
+console.log("push");
+//push(x) -> agrega el elemento al final del array
+fruits.push("fresa");
+console.log(fruits);
+
+console.log("pop");
+//pop()-> elimina el ultimo elemento del array y lo retorna
+fruits.pop();
+console.log(fruits);
