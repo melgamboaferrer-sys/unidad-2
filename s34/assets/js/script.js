d85caf89850d0fe68bfbca82b9fb8daa4543a997
valee_pky <valvillarrealguardado@crackthecode.la>
2025-09-05 09:31:00 -0500

    s34

--- s34/assets/js/script.js
+++ s34/assets/js/script.js
@@ -12,3 +12,24 @@ console.log("pop");
 //pop()-> elimina el ultimo elemento del array y lo retorna
 fruits.pop();
 console.log(fruits);
+
+console.log("unshift");
+//unshift(x) -> agrega el elemento al inicio del array
+fruits.unshift("uva");
+console.log(fruits);
+
+console.log("shift");
+//shift() -> elimina el primer elemento del array y lo retorna
+let delateditem = fruits.shift();
+console.log(`elemento eliminado con shift: ${delateditem}`);
+console.log(fruits);
+
+console.log("includes");
+//includes(x) -> preguntar si x es un elemento del array0. Retorna un buleano
+console.log(fruits.includes("naranja")); //true
+console.log(fruits.includes("kiwi")); //false
+
+console.log("indexOf");
+//includes(x) -> preguntar si x es un elemento del array0. Retorna un buleano
+console.log(fruits.indexOf("naranja")); // 1
+console.log(fruits.indexOf("kiwi")); // -1
